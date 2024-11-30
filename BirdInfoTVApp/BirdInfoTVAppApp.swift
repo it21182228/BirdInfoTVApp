d157bd43a8f29daeb908a0ec137017e7ce735bfb
Sahasrika <it21312076@my.sliit.lk>
2024-11-30 13:25:13 +0530

    Initial Commit

---
+++ BirdInfoTVApp/BirdInfoTVAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirdInfoTVAppApp.swift
+//  BirdInfoTVApp
+//
+//  Created by Anuththara Divarathna on 2024-11-30.
+//
+
+import SwiftUI
+
+@main
+struct BirdInfoTVAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
